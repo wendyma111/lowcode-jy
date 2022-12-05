a73d467eb759c8495906685d6ecb9c9784c4e69e
wendyma111 <2641616227@qq.com>
2022-12-05 14:50:09 +0800

    feat: 更新docs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,24 @@
-const ghpages = require('gh-pages')
+const fs = require('fs-extra')
+const path = require('path')
+const child_process = require('child_process')
 
-ghpages.publish(
-  'build',
-  {
-    repo: 'https://github.com/wendyma111/lowcode-jy.git'
-  },
-  function(err) {
-    console.log(err)
+// 执行打包
+child_process.execSync(
+  `node ${path.resolve(__dirname, './build.js')}`,
+  { encoding: 'utf-8' },
+  (err, stdout) => {
+    if (err) {
+      console.log(err.stack)
+      console.log('Error code: ', err.code)
+      console.log('Signal received ', err.signal)
+      process.exit()
+    }
+  
+    console.log(stdout)
   }
-);
+)
+
+// 将打包后的文件复制到docs
+fs.copySync(path.resolve(__dirname, '../build'), path.resolve(__dirname, '../docs'))
+
+console.log('打包后文件已复制到docs文件夹下')
